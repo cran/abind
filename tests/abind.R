4dab2eac71f78366284fc8d83abffa2e0a0e1a67
Tony Plate <tplate@attglobal.net>
2003-06-10 00:00:00 +0000

    version 1.0-1

--- tests/abind.R
+++ tests/abind.R
@@ -10,7 +10,7 @@ abind(x=1:4,matrix(5:20,nrow=4),along=1)
 abind(1:4,matrix(5:20,nrow=4),along=1)
 # different default dimnames:
 abind(x=1:4,matrix(5:20,nrow=4),along=1)
-abind(x=1:4,matrix(5:20,nrow=4),along=1,force.array=F)
+abind(x=1:4,matrix(5:20,nrow=4),along=1,force.array=FALSE)
 # concatenates two vectors:
 abind(x=1:4,y=5:8)
 abind(x=c(a=1,b=2),y=3:4)
@@ -19,7 +19,7 @@ abind(x=c(a=1,b=2),y=c(c=3,d=4))
 # (1) easiest way: insert new dimension before 1 (use any number less than 1 for along)
 abind(x=1:4,y=5:8,along=0.5)
 abind(x=c(a=1,b=2),y=c(c=3,d=4), along=0) # with names
-abind(x=c(a=1,b=2),y=c(c=3,d=4), along=0, use.first.dimnames=T)
+abind(x=c(a=1,b=2),y=c(c=3,d=4), along=0, use.first.dimnames=TRUE)
 # (2) permute the result:
 aperm(abind(1:4,5:8,along=2),c(2,1))
 # different default dimnames:
@@ -35,14 +35,14 @@ abind(x=matrix(1:16,nrow=4),y=matrix(17:32,nrow=4),along=1.5)
 # examples with three matrices
 cc <- as.data.frame(matrix(25:36,nrow=3))
 aa <- matrix(1:12,nrow=3,dimnames=list(letters[1:3],LETTERS[1:4]))
-abind(a=aa, cc, matrix(25:36,3,4), along=0, use.first.dimnames=T)
-abind(a=aa, cc, matrix(25:36,3,4), along=1, use.first.dimnames=T)
-abind(a=aa, cc, matrix(25:36,3,4), along=1.1, use.first.dimnames=T)
+abind(a=aa, cc, matrix(25:36,3,4), along=0, use.first.dimnames=TRUE)
+abind(a=aa, cc, matrix(25:36,3,4), along=1, use.first.dimnames=TRUE)
+abind(a=aa, cc, matrix(25:36,3,4), along=1.1, use.first.dimnames=TRUE)
 abind(a=aa, cc, matrix(25:36,3,4), along=2)
-abind(a=aa, cc, matrix(25:36,3,4), along=2, use.first.dimnames=T)
-abind(a=aa, cc, matrix(25:36,3,4), along=3, use.first.dimnames=T)
-abind(a=aa, cc, matrix(25:36,3,4), along=3, use.anon.names=F, use.first.dimnames=T)
-abind(a=aa, cc, dd=matrix(25:36,3,4), along=1.1, use.first.dimnames=T)
+abind(a=aa, cc, matrix(25:36,3,4), along=2, use.first.dimnames=TRUE)
+abind(a=aa, cc, matrix(25:36,3,4), along=3, use.first.dimnames=TRUE)
+abind(a=aa, cc, matrix(25:36,3,4), along=3, use.anon.names=FALSE, use.first.dimnames=TRUE)
+abind(a=aa, cc, dd=matrix(25:36,3,4), along=1.1, use.first.dimnames=TRUE)
 x1 <- array(1:8,dim=c(2,2,2),dimnames=list(letters[6:7],letters[1:2],letters[24:25]))
 x1
 # test that we get dimnames correctly when we need to expand dimensions
